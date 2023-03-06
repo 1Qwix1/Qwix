2a7f26fd151ba9374ac1b5f02058e3db3a10e56f
1Qwix1 <106161504+1Qwix1@users.noreply.github.com>
2023-03-06 19:46:11 +0300

    Add files via upload

---
+++ task1/task1/processing.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+#pragma once
+
+vector <double> initialization(string file_name);
+int solve_out(string file_name, vector <double> index);
+
