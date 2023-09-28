6153f7d1c567533a5133b9c2bf03f85ed4a21522
1Qwix1 <106161504+1Qwix1@users.noreply.github.com>
2023-09-28 14:04:21 +0300

    task 3

---
+++ recolor.cpp
@@ -0,0 +1,8 @@
+﻿#include "recolor.h"
+
+ReColor::ReColor(color const& prev) { prevColor = prev; is[RECOLOR] = true; }
+
+
+color ReColor::getColor() const { return prevColor; }
+void ReColor::reColor() {}
+void ReColor::reColor(color const& newColor) {}
